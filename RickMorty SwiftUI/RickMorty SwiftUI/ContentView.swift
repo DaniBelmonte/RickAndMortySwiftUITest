51b2c563684fa641d09e04a17be4ed298f2ed622
daniRudo <dani.belmonte.valero@gmail.com>
2022-12-26 19:55:02 +0100

    Add picker and update AsyncImage
    Solution "Hit maximum timestamp count, will start dropping events", remove asyncImage and add Image to call load image .onAppear

--- RickMorty SwiftUI/RickMorty SwiftUI/ContentView.swift
+++ RickMorty SwiftUI/RickMorty SwiftUI/ContentView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel = CharactersViewModel()
+    @State private var selectedGender: String = "all"
 
     var body: some View {
-        List(viewModel.characters, id: \.id) { character in
-            CharacterRow(character: character)
-                .onAppear {
-                    if character.id == viewModel.characters.last?.id {
-                        viewModel.fetchCharacters()
+        VStack {
+            Picker(selection: $selectedGender, label: Text("Filter by gender")) {
+                Text("All").tag("all")
+                Text("Male").tag("Male")
+                Text("Female").tag("Female")
+                Text("unknown").tag("unknown")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal)
+
+            List(viewModel.characters.filter { character in
+                selectedGender == "all" || character.gender == selectedGender
+            }, id: \.id) { character in
+                CharacterRow(character: character)
+                    .onAppear {
+                        if character.id == viewModel.characters.last?.id {
+                            viewModel.fetchCharacters()
+                        }
                     }
-                }
+            }
+            .onAppear(perform: viewModel.fetchCharacters)
+            .overlay(activityIndicator)
         }
-        .onAppear(perform: viewModel.fetchCharacters)
-        .overlay(activityIndicator)
     }
 
     private var activityIndicator: some View {
@@ -38,22 +52,38 @@ struct ContentView: View {
 
 struct CharacterRow: View {
     let character: Character
+    @State private var image: UIImage?
 
     var body: some View {
-        HStack() {
+        HStack {
             Text(character.name)
                 .font(.headline)
             Spacer()
-            AsyncImage(
-                url: URL(string: character.image),
-                content: { image in
-                    image.resizable()
-                        .frame(width: 50, height: 50)
-                },
-                placeholder: {
-                    ProgressView()
-                })
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            } else {
+                ProgressView()
+            }
         }
+        .onAppear {
+            if image == nil {
+                loadImage()
+            }
+        }
+    }
+
+    private func loadImage() {
+        guard let url = URL(string: character.image) else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+            }
+        }
+        task.resume()
     }
 }
 
